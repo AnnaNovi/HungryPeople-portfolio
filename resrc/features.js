d686f048bb4a600360fc16bc9c52d49430f676dd
AnnaNovi <77693820+AnnaNovi@users.noreply.github.com>
2021-04-06 18:54:54 +0300

    Add files via upload

--- resrc/features.js
+++ resrc/features.js
@@ -1,22 +1,24 @@
 function openbox(id) {
 	var all = document.querySelectorAll(".block-of-text");
 	for (var i = 0; i < all.length; i++) {
-	if (all[i].id == id) {
-	all[i].style.display = (all[i].style.display == 'none')? 'block' : 'none';
-	} else {
-	all[i].style.display = 'none';
+		if (all[i].id == id) {
+		all[i].style.display = (all[i].style.display == 'none')? 'block' : 'none';
+		} else {
+		all[i].style.display = 'none';
+		}
 	}
-}
 }
 
-function special(id) {
-var all = document.querySelectorAll(".block");
-for (var i = 0; i < all.length; i++) {
-	if (all[i].id == id) {
-	all[i].style.display = (all[i].style.display == 'none')? 'block' : 'none';
-	}
-	else {
-	all[i].style.display = 'none';
-	}
-}
-}
+$(document).ready(function(){
+    $('.description').slick({
+		dots: true,
+		initialSlide: 1
+    });
+});
+
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
