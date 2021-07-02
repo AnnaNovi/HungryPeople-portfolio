b46798eb7beabbec811e09f2b11f42b01da347e8
AnnaNovi <77693820+AnnaNovi@users.noreply.github.com>
2021-07-02 14:41:24 +0300

    Add files via upload

--- resrc/features.js
+++ resrc/features.js
@@ -9,16 +9,29 @@ function openbox(id) {
 	}
 }
 
-$(document).ready(function(){
-    $('.description').slick({
-		dots: true,
-		initialSlide: 1
-    });
+var swiper = new Swiper(".foodSwiper", {
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true
+	},
+	initialSlide: 1
 });
 
 $(document).ready(function(){
 	$('.header__burger').click(function(event){
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
+	});
+});
+
+$(document).ready(function() {
+	$("a.scroll").click(function() {
+		$("html, body").animate({
+			scrollTop: $($(this).attr("href")).offset().top + "px"
+		}, {
+			duration: 500,
+			easing: "swing"
+		});
+		return false;
 	});
 });
